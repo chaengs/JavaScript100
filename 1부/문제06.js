16cb283033da002b020eb8ad73a0060ef682044b
chaeng <cathyshim222@naver.com>
2021-12-02 16:45:11 +0900

    4-9번 풀이

---
+++ 1부/문제06.js
@@ -0,0 +1,2 @@
+//false인 것 : NaN, 빈 문자열, 0, null, undefined, false
+//그 외는 참
---
+++ 1부/문제08.js
@@ -0,0 +1,10 @@
+var d = {
+    'height':180,
+    'weight':78,
+    'weight':84,
+    'temperature':36,
+    'eyesight':1
+  };
+  console.log(d['weight']);
+  //답 84
+  //객체의 키 값이 중복되어 할당되면 가장 최근에 할당 된 것이 기준
